48ba4c33720722f3e46011dfc13684587f1562d0
sairam srirangam <sairam.srirangam@investisdigital.com>
2022-07-26 01:39:57 +0100

    new changes

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,13 +1,34 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Enter number of students in class that took exam");
-        int noOfStudents = scanner.nextInt();
-        int[] marks = studentMarks(noOfStudents);
-        double avg = averageMark(marks);
-        System.out.println("Average mark of class is " + avg);
+//        System.out.println("Enter number of students in class that took exam");
+//        int noOfStudents = scanner.nextInt();
+//        int[] marks = studentMarks(noOfStudents);
+//        double avg = averageMark(marks);
+//        System.out.println("Average mark of class is " + avg);
+        // test code below
+
+        Test[] testers = new Test[10];
+        Test testerOne = new Test("sai", "white box tester");
+        Test testerTwo = new Test("ram", "black box tester");
+        testers[0] = testerOne;
+        testers[1] = testerOne;
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Sai");
+        arrayList.add("ram");
+        arrayList.add("aaa");
+        arrayList.add("bbb");
+        arrayList.add(1,"ccc");
+        Iterator<String> i = arrayList.iterator();
+//        ListIterator<String> j = arrayList.listIterator();
+//        j.previous();
+        while (i.hasNext())
+            System.out.println(i.next());
     }
     public static int[] studentMarks(int noOfStudents) {
         int[] studentMarks = new int[noOfStudents];
---
+++ Arrays/src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public String testerName;
+    public String testerDesignation;
+
+    public Test(String testerName, String testerDesignation) {
+        this.testerName = testerName;
+        this.testerDesignation = testerDesignation;
+    }
+
+    public String getTesterName() {
+        return testerName;
+    }
+
+    public String getTesterDesignation() {
+        return testerDesignation;
+    }
+}
